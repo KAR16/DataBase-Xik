8420876d41d753779e8da3b7075d4b09f9104032
KAR16 <kherrera16@gmail.com>
2016-05-24 16:42:30 -0600

    Trigger Insert Plan Cargo V. 1

--- Codigo Adicional.sql
+++ Codigo Adicional.sql
@@ -1,4 +1,4 @@
---Codigo Adicional 
+--Codigo Adicional PLAN PAGO
 
 --Cración de Secuencias
 create sequence Cont_PlanPago;
@@ -10,3 +10,32 @@ create sequence Carne;
 Cont_PlanPago.NEXTVAL;
 Cont_UserCargo.NEXTVAL;
 Carne.NEXTVAL;
+
+
+--CODIGO SOBRE EL INSERT CARGO 
+SELECT *
+FROM CARGO;
+
+SELECT *
+FROM PLAN_PAGO;
+
+INSERT INTO PLAN_PAGO
+VALUES(Cont_PlanPago.NEXTVAL,('201600' || Carne.NEXTVAL),SYSDATE, 50000, 50);
+
+delete from Cargo;
+delete from Plan_Pago;
+Commit;
+
+
+--Codigo Adicional PAGO
+
+--Creación de Secuencias
+CREATE SEQUENCE SECIdPago;
+Commit;
+
+--CÓDIGO SOBRE EL INSERT PAGO
+INSERT INTO PAGO
+VALUES(SECIdPago.NEXTVAL, '2016001', SYSDATE, 3000, 'Pago 1');
+
+SELECT *
+FROM PAGO;
