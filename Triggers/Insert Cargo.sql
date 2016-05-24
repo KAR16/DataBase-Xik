24bc9ada24cb3dcce7ae313975a1663b03ed0a70
KAR16 <kherrera16@gmail.com>
2016-05-24 09:12:25 -0600

    Trigger Insert Cargo V.2 Se modificaron las fechas y la lógica de inserción de las mismas

--- Triggers/Insert Cargo.sql
+++ Triggers/Insert Cargo.sql
@@ -4,6 +4,8 @@ AFTER INSERT ON PLAN_PAGO
 DECLARE 
 IDPlanPago INT := (:NEW.Plan_Pago); Monto INT := 0; NoCuotas INT := 0; Cuota INT := 0; Fecha VARCHAR(10); Cont INT := 1;
 Mes INT := 0; Anio INT := 0;
+Fecha_Transaccion DATE;
+Fecha_Vencimiento DATE;
 
 --FINAL DECLARE
 BEGIN
@@ -17,8 +19,8 @@ NoCuotas := (:NEW.Cuotas);
 Cuota := (Monto / NoCuotas);
 --HACEMOS LA OPERACION PARA LA FECHA
 
-Anio := (select to_char(sysdate,'YYYY') from dual); 
-Mes := (select to_char(sysdate,'MM') from dual);
+Anio := (EXTRACT(YEAR FROM SYSDATE)); 
+Mes := (EXTRACT(MONTH FROM SYSDATE));
 Fecha := CONTACT(Anio, '/', (Mes + 1), '/05');
 
 --AHORA INSERTAMOS EN CARGOS LAS CUOTAS
@@ -28,17 +30,15 @@ Cuota, Cuota, Fecha, CONCAT('Cuota ', Cont));
 
 
 --ACA INICIAMOS EL WHILE PARA TERMINAR DE INSERTAR LAS CUTOAS RESTANTES
-WHILE (Cont < NoCuotas)
-BEGIN
-
+WHILE (Cont < (NoCuotas - 1))
+LOOP
 --AQUI MODIFICAMOS LAS FECHAS DE TRANSACCION Y VENCIMIENTO PARA LAS CUOTAS
-DECLARE Fecha_Transaccion DATE := CONCAT(Anio, '/', (Mes + Cont), '/06');
-DECLARE Fecha_Vencimiento DATE := CONCAT(Anio, '/', ((Mes + 1)+ Cont), '/05');
+Fecha_Transaccion := CONCAT(Anio, '/', (Mes + Cont), '/06');
+Fecha_Vencimiento := CONCAT(Anio, '/', ((Mes + 1)+ Cont), '/05');
 
 INSERT INTO Cargo 
 VALUES(Cont, (:NEW.Carne), Fecha_Transaccion, Cuota, Cuota, Fecha_Vencimiento, CONCAT('Cuota', Cont));
-SET Cont += 1; --AQUI INCREMENTAMOS EL VALOR DEL LOOP DE 1-1
-END
+Cont := Cont + 1; --AQUI INCREMENTAMOS EL VALOR DEL LOOP DE 1-1
+END LOOP;
   NULL;
 END;
-
