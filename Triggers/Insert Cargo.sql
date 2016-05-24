ef2a225d59cb7c80344ffd5ce8b804f04d65ac0a
KAR16 <kherrera16@gmail.com>
2016-05-24 11:51:43 -0600

    Trigger V. 4 Final

--- Triggers/Insert Cargo.sql
+++ Triggers/Insert Cargo.sql
@@ -1,4 +1,4 @@
-CREATE or REPLACE TRIGGER INSERTCARGO 
+create or replace TRIGGER INSERTCARGO 
 AFTER INSERT ON PLAN_PAGO
 FOR EACH ROW
 DECLARE 
@@ -7,7 +7,6 @@ Fecha_Final DATE; Cont INT := 1;
 Mes INT := 0; Anio INT := 0;
 Fecha_Transaccion DATE;
 Fecha_Vencimiento DATE;
-ContAnio INT := 1;
 
 --FINAL DECLARE
 BEGIN
@@ -25,64 +24,55 @@ Anio := (EXTRACT(YEAR FROM SYSDATE));
 
 --Fecha_Final := TO_DATE(Fecha, 'YYYY/MON/DD');
 Mes := (EXTRACT(MONTH FROM SYSDATE));
-IF Mes > 12 THEN
-    Mes := 1;
-    Anio:= Anio + ContAnio;
-    IF (Mes + 1) <= 9 THEN
-    Fecha := TO_DATE(Anio|| '/0' || (Mes + 1) || '/05', 'YYYY/MM/DD');
-    ELSE
-    Fecha := TO_DATE(Anio|| '/' || (Mes + 1) || '/05', 'YYYY/MM/DD');
-    END IF;
-ELSE
-    IF (Mes + 1 )<= 9 THEN
-    Fecha := TO_DATE(Anio|| '/0' || (Mes + 1) || '/05', 'YYYY/MM/DD');
-    ELSE
-    Fecha := TO_DATE(Anio|| '/' || (Mes + 1) || '/05', 'YYYY/MM/DD');
-    END IF;
-END IF;
+
+  IF (Mes) <= 9 THEN
+  Fecha := TO_DATE(Anio|| '/0' || (Mes + 1) || '/05', 'YYYY/MM/DD');
+  ELSE
+  Fecha := TO_DATE(Anio|| '/' || (Mes + 1) || '/05', 'YYYY/MM/DD');
+  END IF;
 
 --AHORA INSERTAMOS EN CARGOS LAS CUOTAS
 INSERT INTO Cargo
 VALUES(cont_usercargo.NEXTVAL, (:NEW.Carne), (:NEW.Fecha_Aplicacion), Cuota, Cuota, Fecha, ('Cuota ' || Cont));
 
-
 --ACA INICIAMOS EL WHILE PARA TERMINAR DE INSERTAR LAS CUTOAS RESTANTES
 WHILE (Cont < NoCuotas) LOOP
 --AQUI MODIFICAMOS LAS FECHAS DE TRANSACCION Y VENCIMIENTO PARA LAS CUOTAS
+Mes:= Mes + 1;
+
 IF Mes > 12 THEN
     Mes := 1;
-    Anio:= Anio + ContAnio;
+    Anio:= Anio + 1;
     IF (Mes + Cont) <= 9 THEN
-    Fecha_Transaccion := TO_DATE(Anio || '/0' || (Mes + Cont)|| '/06', 'YYYY/MM/DD' );
+    Fecha_Transaccion := TO_DATE(Anio || '/0' || (Mes)|| '/06', 'YYYY/MM/DD' );
     ELSE
-    Fecha_Transaccion := TO_DATE(Anio || '/' || (Mes + Cont)|| '/06', 'YYYY/MM/DD' );
+    Fecha_Transaccion := TO_DATE(Anio || '/' || (Mes )|| '/06', 'YYYY/MM/DD' );
     END IF;
-    ContAnio := ContAnio + 1;
 ELSE
-    IF (Mes + Cont) <= 9 THEN
-    Fecha_Transaccion := TO_DATE(Anio || '/0' || (Mes + Cont)|| '/06', 'YYYY/MM/DD' );
+    IF (Mes) <= 9 THEN
+    Fecha_Transaccion := TO_DATE(Anio || '/0' || (Mes)|| '/06', 'YYYY/MM/DD' );
     ELSE
-    Fecha_Transaccion := TO_DATE(Anio || '/' || (Mes + Cont)|| '/06', 'YYYY/MM/DD' );
+    Fecha_Transaccion := TO_DATE(Anio || '/' || (Mes)|| '/06', 'YYYY/MM/DD' );
     END IF;
 END IF;
 
 
-IF Mes > 12 THEN
-    Mes := 1;
-    Anio:= Anio + ContAnio;
+IF (EXTRACT(MONTH FROM Fecha_Transaccion)) >= 12 THEN
+    Mes := 0;
+    Anio:= Anio + 1 ;
     IF (Mes + Cont + 1)<=9 THEN
-    Fecha_Vencimiento := TO_DATE(Anio || '/0' || ((Mes + 1) + Cont) || '/05', 'YYYY/MM/DD');
+    Fecha_Vencimiento := TO_DATE(Anio || '/0' || (Mes + 1) || '/05', 'YYYY/MM/DD');
     ELSE
-    Fecha_Vencimiento := TO_DATE(Anio || '/' || ((Mes + 1) + Cont) || '/05', 'YYYY/MM/DD');
+    Fecha_Vencimiento := TO_DATE(Anio || '/' || (Mes + 1) || '/05', 'YYYY/MM/DD');
     END IF;
-    ContAnio := ContAnio + 1;
 ELSE
     IF (Mes + Cont + 1)<=9 THEN
-    Fecha_Vencimiento := TO_DATE(Anio || '/0' || ((Mes + 1) + Cont) || '/05', 'YYYY/MM/DD');
+    Fecha_Vencimiento := TO_DATE(Anio || '/0' || (Mes + 1) || '/05', 'YYYY/MM/DD');
     ELSE
-    Fecha_Vencimiento := TO_DATE(Anio || '/' || ((Mes + 1) + Cont) || '/05', 'YYYY/MM/DD');
+    Fecha_Vencimiento := TO_DATE(Anio || '/' || (Mes + 1) || '/05', 'YYYY/MM/DD');
     END IF;
 END IF;
+
 Cont := Cont + 1; --AQUI INCREMENTAMOS EL VALOR DEL LOOP DE 1-1
 INSERT INTO Cargo 
 VALUES(cont_usercargo.NEXTVAL, (:NEW.Carne), Fecha_Transaccion, Cuota, Cuota, Fecha_Vencimiento, ('Cuota ' || Cont));
