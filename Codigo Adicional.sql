38dabeee815fe5b241869e451ee7e9247c26b824
KAR16 <kherrera16@gmail.com>
2016-05-24 09:48:37 -0600

    Codigo Adicional

---
+++ Codigo Adicional.sql
@@ -0,0 +1,12 @@
+--Codigo Adicional 
+
+--Cración de Secuencias
+create sequence Cont_PlanPago;
+create sequence Cont_UserCargo;
+create sequence Carne;
+
+
+--Como Iterar sobre las secuencias
+Cont_PlanPago.NEXTVAL;
+Cont_UserCargo.NEXTVAL;
+Carne.NEXTVAL;
